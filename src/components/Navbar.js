309cfdbfc082b5d73951625485e50457413c084b
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-11-03 10:57:46 +0200

    feat: done

---
+++ src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import logo from "../images/logo.svg";
+import { pageLinks } from "../data";
+import PageLinks from "./PageLinks";
+import { icons } from "../data";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="nav-center">
+                <div className="nav-header">
+                    <a href="#home">
+                        <img src={logo} className="nav-logo" alt="backroads" />
+                    </a>
+                    <button
+                        type="button"
+                        className="nav-toggle"
+                        id="nav-toggle"
+                    >
+                        <i className="fas fa-bars"></i>
+                    </button>
+                </div>
+
+                <PageLinks parentClass="nav-links" itemClass="nav-link" />
+
+                <ul className="nav-icons">
+                    {icons.map((link) => {
+                        const { id, href, icon } = link;
+                        return (
+                            <li key={id}>
+                                <a
+                                    href={href}
+                                    target="_blank"
+                                    className="nav-icon"
+                                    rel="noreferrer"
+                                >
+                                    <i className={icon}></i>
+                                </a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,104 @@
+import img1 from './images/tour-1.jpeg';
+import img2 from './images/tour-2.jpeg';
+import img3 from './images/tour-3.jpeg';
+import img4 from './images/tour-4.jpeg';
+import img5 from './images/tour-5.jpeg';
+import img6 from './images/tour-6.jpeg';
+
+export const pageLinks = [
+    { id: 1, href: "#home", text: "home" },
+    { id: 2, href: "#about", text: "about" },
+    { id: 3, href: "#services", text: "services" },
+    { id: 4, href: "#tours", text: "tours" },
+];
+
+export const icons = [
+    { id: 1, href: "https://www.facebook.com", icon: "fab fa-facebook" },
+    { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+    { id: 3, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
+];
+
+export const services = [
+    {
+        id: 1,
+        title: "saving money",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicingelit. Asperiores, officia.",
+        icon: "fas fa-wallet fa-fw",
+    },
+    {
+        id: 2,
+        title: "endless hiking",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicingelit. Asperiores, officia.",
+        icon: "fas fa-tree fa-fw",
+    },
+    {
+        id: 3,
+        title: "amazing comfort",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicingelit. Asperiores, officia.",
+        icon: "fas fa-socks fa-fw",
+    },
+];
+
+export const tours = [
+    {
+        id: 1,
+        date: "august 26th, 2023",
+        img: img1,
+        title: "Tibet Adventure",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicingelit. Cumque vitae tempore voluptatum maximereprehenderit eum quod exercitationem fugit, quicorporis.",
+        country: "china",
+        days: 6,
+        price: 2100,
+    },
+    {
+        id: 2,
+        date: "october 1th, 2023",
+        img: img2,
+        title: "Haifa Adventure",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicingelit. Cumque vitae tempore voluptatum maximereprehenderit eum quod exercitationem fugit, quicorporis.",
+        country: "israel",
+        days: 11,
+        price: 1400,
+    },
+    {
+        id: 3,
+        date: "septemmber 15th, 2023",
+        img: img3,
+        title: "Explore Hong Kong",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicingelit. Cumque vitae tempore voluptatum maximereprehenderit eum quod exercitationem fugit, quicorporis.",
+        country: "hong kong",
+        days: 8,
+        price: 5000,
+    },
+    {
+        id: 4,
+        date: "december 5th, 2023",
+        img: img4,
+        title: "Kenya highlights",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicingelit. Cumque vitae tempore voluptatum maximereprehenderit eum quod exercitationem fugit, quicorporis.",
+        country: "kenya",
+        days: 20,
+        price: 3300,
+    },
+    {
+        id: 5,
+        date: "oktober 5th, 2023",
+        img: img5,
+        title: "Malaziya",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicingelit. Cumque vitae tempore voluptatum maximereprehenderit eum quod exercitationem fugit, quicorporis.",
+        country: "Malaziya",
+        days: 15,
+        price: 2000,
+    },
+    {
+        id: 6,
+        date: "march 5th, 2024",
+        img: img6,
+        title: "Turkey weekend",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicingelit. Cumque vitae tempore voluptatum maximereprehenderit eum quod exercitationem fugit, quicorporis.",
+        country: "Turkey",
+        days: 3,
+        price: 300,
+    },
+    
+];
